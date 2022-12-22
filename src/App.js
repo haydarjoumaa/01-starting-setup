848ec90eecb13ee0283efa7ac8152b761dcecb39
haydar <haydarjoumaa@gmail.com>
2022-12-22 12:57:15 +0200

    hio

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Expenseitem from "./components/Expenseitem";
+
+function App() {
+  return (
+    <div>
+      <h2>Let's get started!</h2><p>hi</p>
+      <Expenseitem/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Expenseitem.js
@@ -0,0 +1,6 @@
+function Expenseitem(){
+    return <h1>this is expense!</h1>
+    
+}
+
+export default Expenseitem
